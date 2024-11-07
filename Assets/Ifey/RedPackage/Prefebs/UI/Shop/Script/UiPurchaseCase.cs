4bc7c8815f7302835e4dbc76ad758e84a4a62131
MouseHuan <0522weisihuan@gmail.com>
2024-11-07 10:51:37 +0800

    no message

--- Assets/Ifey/RedPackage/Prefebs/UI/Shop/Script/UiPurchaseCase.cs
+++ Assets/Ifey/RedPackage/Prefebs/UI/Shop/Script/UiPurchaseCase.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.UI;
+using Vuplex.WebView;
 public class UiPurchaseCase : Singleton_Base<UiPurchaseCase>
 {
     public override bool isDontDestroy => false;
@@ -12,6 +14,25 @@ public class UiPurchaseCase : Singleton_Base<UiPurchaseCase>
     public Image loading_Image;
     public Text countDown_Text;
     public long targetStampTime;
+    public Transform webgl_Case;
+    public CanvasWebViewPrefab mobile_Case;
+    public string targetUrl = "";
+    private async void Start()
+    {
+       
+#if UNITY_WEBGL
+        webgl_Case.gameObject.SetActive(true);
+        mobile_Case.gameObject.SetActive(false);
+#elif UNITY_EDITOR || PLATFORM_ANDROID
+        webgl_Case.gameObject.SetActive(false);
+        mobile_Case.gameObject.SetActive(true);
+        await mobile_Case.WaitUntilInitialized();
+        if(targetUrl!= mobile_Case.WebView.Url)
+        {
+            mobile_Case.WebView.LoadUrl(targetUrl);
+        }
+#endif
+    }
     private void Update()
     {
         if (loading_Image != null && loading_Image.gameObject.activeSelf)
@@ -32,22 +53,40 @@ public class UiPurchaseCase : Singleton_Base<UiPurchaseCase>
     protected override void Awake()
     {
         base.Awake();
-        _case.gameObject.SetActive(false);
     }
     public void OpenUrl(string url)
     {
-        webIframe_Ctrl.SetUrl(url);
+        targetUrl = url;
         _case.gameObject.SetActive(true);
+#if UNITY_WEBGL
+        webIframe_Ctrl.SetUrl(url);
+#elif UNITY_EDITOR || PLATFORM_ANDROID
+        mobile_Case?.WebView?.LoadUrl(url);
+#endif
     }
     public void Close()
     {
         _case?.gameObject?.SetActive(false);
+
+#if UNITY_WEBGL
+#elif UNITY_EDITOR || PLATFORM_ANDROID
+        mobile_Case?.WebView?.LoadUrl("about:blank");
+        //mobile_Case?.WebView?.Dispose();  // Çå³ý»º´æ
+#endif
+
         closeAction?.Invoke();
         closeAction = null;
     }
     protected override void OnDestroy()
     {
         base.OnDestroy();
+
+#if UNITY_WEBGL
+#elif UNITY_EDITOR || PLATFORM_ANDROID
+        mobile_Case?.WebView?.LoadUrl("about:blank");
+        mobile_Case?.WebView?.Dispose();  // Çå³ý»º´æ
+#endif
+
         closeAction?.Invoke();
         closeAction = null;
     }
---
+++ Assets/Vibration/Scripts/VibrationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+using MoreMountains.NiceVibrations;
+
+namespace Fishtail.PlayTheBall.Vibration
+{
+    public class VibrationController : MonoBehaviour
+    {
+        public static VibrationController instance { get; private set; }
+
+        public bool vibrate { get; set; }
+
+        private void Awake()
+        {
+            instance = this;
+
+            MMVibrationManager.iOSInitializeHaptics();
+        }
+
+        private void OnDestroy()
+        {
+			MMVibrationManager.iOSReleaseHaptics();
+        }
+
+        public void ImpactLight()
+        {
+            if (!vibrate) { return; }
+            MMVibrationManager.Haptic(HapticTypes.LightImpact);
+        }
+        public void ImpactHeavy()
+        {
+            if (!vibrate) { return; }
+            MMVibrationManager.Haptic(HapticTypes.HeavyImpact);
+        }
+        public void ImpactMedium()
+        {
+            if (!vibrate) { return; }
+            MMVibrationManager.Haptic(HapticTypes.MediumImpact);
+        }
+    }
+}
